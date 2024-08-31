4cea036edb9683917c2afe4239378c4399d97de6
nguyn_huynh <huynhphuocnguyen2412@gmail.com>
2024-09-01 02:51:26 +0700

    clean: login:

--- src/components/Login/GoogleLoginComponent.jsx
+++ src/components/Login/GoogleLoginComponent.jsx
@@ -5,24 +5,31 @@ import { store } from "../../redux/store";
 import { useNavigate } from "react-router-dom";
 
 const GoogleLoginComponent = () => {
-  const navigate = useNavigate();
-  const onLoginGoogleSuccess = async (res) => {
-    const { data, isOk } = await authApi.loginByGoogle(res.credential);
-    if (isOk) {
-      const { accessToken, refreshToken } = data;
-      store.dispatch(authActions.setCredentials({ accessToken, refreshToken }));
-      navigate("/");
-    }
-  };
-  const onLoginGoogleFailure = async (res) => {
-    console.log(res);
-  };
-  return (
-    <GoogleLogin
-      onSuccess={onLoginGoogleSuccess}
-      onError={onLoginGoogleFailure}
-    />
-  );
+    const navigate = useNavigate();
+    const onLoginGoogleSuccess = async (res) => {
+        const { data, isOk } = await authApi.loginByGoogle(res.credential);
+        if (isOk) {
+            const { accessToken, refreshToken } = data;
+            store.dispatch(
+                authActions.setCredentials({ accessToken, refreshToken })
+            );
+            navigate("/");
+        }
+    };
+    const onLoginGoogleFailure = async (res) => {
+        console.log(res);
+    };
+    return (
+        <GoogleLogin
+            onSuccess={onLoginGoogleSuccess}
+            onError={onLoginGoogleFailure}
+            style={{
+                width: "100%",
+                borderRadius: 30,
+                padding: "15px 20px",
+            }}
+        />
+    );
 };
 
 export default GoogleLoginComponent;
--- src/components/Login/RegisterForm.jsx
+++ src/components/Login/RegisterForm.jsx
@@ -7,132 +7,136 @@ import authApi from "../../apis/authApi";
 import OTPModal from "../OTPModal/OTPModal";
 
 const RegisterForm = () => {
-  const navigate = useNavigate();
-  const { isLoading, fetchData, contextHolder } = useFetch();
-  const [isOtpVisible, setIsOtpVisible] = useState(false);
-  const [email, setEmail] = useState("");
+    const navigate = useNavigate();
+    const { isLoading, fetchData, contextHolder } = useFetch();
+    const [isOtpVisible, setIsOtpVisible] = useState(false);
+    const [email, setEmail] = useState("");
 
-  const onFinish = async (values) => {
-    setEmail(values.email);
-    const { email, password, name } = values;
+    const onFinish = async (values) => {
+        setEmail(values.email);
 
-    const { isOk, data } = await fetchData(() =>
-      authApi.register({ email, password, name })
+        const { isOk, data } = await fetchData(() => authApi.register(values));
+
+        if (isOk) {
+            console.log(data);
+            setIsOtpVisible(true);
+        }
+    };
+
+    const handleOtpSuccess = () => {
+        setIsOtpVisible(false);
+        navigate("/login");
+    };
+
+    return (
+        <>
+            {contextHolder}
+            <Form
+                name="register"
+                layout="vertical"
+                style={{
+                    maxWidth: 600,
+                    margin: "0 auto",
+                }}
+                initialValues={{
+                    remember: true,
+                }}
+                onFinish={onFinish}
+                autoComplete="on"
+            >
+                <Form.Item
+                    label="Name"
+                    name="name"
+                    rules={[
+                        {
+                            required: true,
+                            message: "Please input your name!",
+                        },
+                    ]}
+                >
+                    <Input
+                        prefix={<UserOutlined />}
+                        placeholder="Enter your full name"
+                    />
+                </Form.Item>
+
+                <Form.Item
+                    label="Email"
+                    name="email"
+                    rules={[
+                        {
+                            required: true,
+                            type: "email",
+                            message: "Please input a valid email!",
+                        },
+                    ]}
+                >
+                    <Input
+                        prefix={<MailOutlined />}
+                        placeholder="Enter your email"
+                    />
+                </Form.Item>
+
+                <Form.Item
+                    label="Password"
+                    name="password"
+                    rules={[
+                        {
+                            required: true,
+                            message: "Please input your password!",
+                        },
+                    ]}
+                >
+                    <Input.Password
+                        prefix={<LockOutlined />}
+                        placeholder="Enter your password"
+                    />
+                </Form.Item>
+
+                <Form.Item
+                    label="Confirm Password"
+                    name="confirmPassword"
+                    rules={[
+                        {
+                            required: true,
+                            message: "Please confirm your password!",
+                        },
+                    ]}
+                >
+                    <Input.Password
+                        prefix={<LockOutlined />}
+                        placeholder="Repeat password"
+                    />
+                </Form.Item>
+
+                <Form.Item
+                    wrapperCol={{
+                        span: 24,
+                    }}
+                >
+                    <Button
+                        type="primary"
+                        htmlType="submit"
+                        loading={isLoading}
+                        style={{
+                            width: "100%",
+                            borderRadius: 30,
+                            padding: "15px 20px",
+                        }}
+                    >
+                        Register
+                    </Button>
+                </Form.Item>
+            </Form>
+
+            <OTPModal
+                isVisible={isOtpVisible}
+                email={email}
+                handleCloseOtpModal={() => setIsOtpVisible(false)}
+                onSuccess={handleOtpSuccess}
+            />
+        </>
     );
-
-    if (isOk) {
-      setIsOtpVisible(true);
-    }
-  };
-
-  const handleOtpSuccess = () => {
-    setIsOtpVisible(false);
-    navigate("/login");
-  };
-
-  return (
-    <>
-      {contextHolder}
-      <Form
-        name="register"
-        layout="vertical"
-        style={{
-          maxWidth: 600,
-          margin: "0 auto",
-        }}
-        initialValues={{
-          remember: true,
-        }}
-        onFinish={onFinish}
-        autoComplete="on"
-      >
-        <Form.Item
-          label="Name"
-          name="name"
-          rules={[
-            {
-              required: true,
-              message: "Please input your name!",
-            },
-          ]}
-        >
-          <Input prefix={<UserOutlined />} placeholder="Enter your full name" />
-        </Form.Item>
-
-        <Form.Item
-          label="Email"
-          name="email"
-          rules={[
-            {
-              required: true,
-              type: "email",
-              message: "Please input a valid email!",
-            },
-          ]}
-        >
-          <Input prefix={<MailOutlined />} placeholder="Enter your email" />
-        </Form.Item>
-
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: "Please input your password!",
-            },
-          ]}
-        >
-          <Input.Password
-            prefix={<LockOutlined />}
-            placeholder="Enter your password"
-          />
-        </Form.Item>
-
-        <Form.Item
-          label="Confirm Password"
-          name="confirmPassword"
-          rules={[
-            {
-              required: true,
-              message: "Please confirm your password!",
-            },
-          ]}
-        >
-          <Input.Password
-            prefix={<LockOutlined />}
-            placeholder="Repeat password"
-          />
-        </Form.Item>
-
-        <Form.Item
-          wrapperCol={{
-            span: 24,
-          }}
-        >
-          <Button
-            type="primary"
-            htmlType="submit"
-            loading={isLoading}
-            style={{
-              width: "100%",
-              borderRadius: 30,
-              padding: "15px 20px",
-            }}
-          >
-            Register
-          </Button>
-        </Form.Item>
-      </Form>
-
-      <OTPModal
-        isVisible={isOtpVisible}
-        email={email}
-        handleCloseOtpModal={() => setIsOtpVisible(false)}
-        onSuccess={handleOtpSuccess}
-      />
-    </>
-  );
 };
 
 export default RegisterForm;
