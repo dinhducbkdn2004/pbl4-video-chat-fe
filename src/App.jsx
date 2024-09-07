5c95a5810a9a673329ec9a087ce6d07aae1590c4
nguyn_huynh <huynhphuocnguyen2412@gmail.com>
2024-09-07 15:15:29 +0700

    init search page

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,30 @@ import MainPage from "./pages/MainPage/MainPage";
 import MessagePage from "./pages/MessagePage/MessagePage";
 import ContactPage from "./pages/ContactPage/ContactPage";
 import ForgotPassword from "./pages/LoginPage/ForgotPasswordPage";
+import SearchPage from "./pages/SearchPage";
 
 function App() {
-  return (
-    <Routes>
-      {/* Public route for unauthenticated users */}
-      <Route element={<UnauthorizeRoute />}>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-      </Route>
+    return (
+        <Routes>
+            {/* Public route for unauthenticated users */}
+            <Route element={<UnauthorizeRoute />}>
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/forgot-password" element={<ForgotPassword />} />
+            </Route>
 
-      {/* Protected route for authenticated users */}
-      <Route element={<ProtectedRoute />}>
-        <Route path="/" element={<MainPage />}>
-          <Route path="/message" element={<MessagePage />} />
-          <Route path="/contact" element={<ContactPage />} />
-        </Route>
-      </Route>
+            {/* Protected route for authenticated users */}
+            <Route element={<ProtectedRoute />}>
+                <Route path="/" element={<MainPage />}>
+                    <Route path="/message" element={<MessagePage />} />
+                    <Route path="/contact" element={<ContactPage />} />
+                    <Route path="/search" element={<SearchPage />} />
+                </Route>
+            </Route>
 
-      {/* Optional: Catch-all route for undefined paths */}
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
-  );
+            {/* Optional: Catch-all route for undefined paths */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+    );
 }
 
 export default App;
--- src/pages/MainPage/SideBar.jsx
+++ src/pages/MainPage/SideBar.jsx
@@ -10,63 +10,72 @@ import logo from "../../assets/logo.png";
 const { Sider } = Layout;
 
 const Sidebar = () => {
-  return (
-    <Sider
-      width={80}
-      className="sidebar-container"
-      style={{
-        backgroundColor: "#f0f2f5",
-        borderInlineEnd: "1px solid #d9d9d9",
-      }}
-    >
-      <div
-        style={{
-          padding: "10px 0",
-          alignItems: "center",
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
-        <img src={logo} alt="Logo" style={{ width: "40px", height: "40px" }} />
-      </div>
-      <Menu
-        mode="vertical"
-        className="custom-menu"
-        style={{ backgroundColor: "#f0f2f5" }}
-      >
-        <Menu.Item
-          key="chat"
-          icon={<HiOutlineChat size={21} />}
-          className="custom-menu-item"
-        />
-        <Menu.Item
-          key="group"
-          icon={<BiGroup size={21} />}
-          className="custom-menu-item"
-        />
-        <Menu.Item
-          key="call"
-          icon={<IoCallOutline size={21} />}
-          className="custom-menu-item"
-        />
-        <Menu.Item
-          key="contact"
-          icon={<LuContact2 size={21} />}
-          className="custom-menu-item"
-        />
-        <Menu.Item
-          key="setting"
-          icon={<IoSettingsOutline size={21} />}
-          className="custom-menu-item"
-        />
-      </Menu>
-      <div className="avatar-container">
-        <Badge count={5}>
-          <Avatar size={40} src={user} />
-        </Badge>
-      </div>
-    </Sider>
-  );
+    return (
+        <Sider
+            width={80}
+            className="sidebar-container"
+            style={{
+                backgroundColor: "#f0f2f5",
+                borderInlineEnd: "1px solid #d9d9d9",
+            }}
+        >
+            <div
+                style={{
+                    padding: "10px 0",
+                    alignItems: "center",
+                    display: "flex",
+                    justifyContent: "center",
+                }}
+            >
+                <img
+                    src={logo}
+                    alt="Logo"
+                    style={{ width: "40px", height: "40px" }}
+                />
+            </div>
+            <Menu
+                mode="vertical"
+                className="custom-menu"
+                style={{ backgroundColor: "#f0f2f5" }}
+            >
+                <Menu.Item
+                    key="chat"
+                    icon={<HiOutlineChat size={21} />}
+                    className="custom-menu-item"
+                />
+                <Menu.Item
+                    key="group"
+                    icon={<BiGroup size={21} />}
+                    className="custom-menu-item"
+                />
+                <Menu.Item
+                    key="call"
+                    icon={<IoCallOutline size={21} />}
+                    className="custom-menu-item"
+                />
+                <Menu.Item
+                    key="contact"
+                    icon={<LuContact2 size={21} />}
+                    className="custom-menu-item"
+                />
+                <Menu.Item
+                    key="setting"
+                    icon={<IoSettingsOutline size={21} />}
+                    className="custom-menu-item"
+                />
+                <Menu.Item
+                    key="search"
+                    icon={<IoSettingsOutline size={21} />}
+                    className="custom-menu-item"
+                />
+            </Menu>
+            <div className="avatar-container">
+                <Badge count={5}>
+                    <Avatar size={40} src={user} />
+                </Badge>
+            </div>
+        </Sider>
+    );
 };
 
 export default Sidebar;
