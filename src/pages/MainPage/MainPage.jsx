2e454c06b1a6b14c848d746cc2b829a82a65c561
= <dinhducbkdn2004@gmail.com>
2024-09-07 15:05:47 +0700

    chat-list

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,10 +1,6 @@
-import { ContactsOutlined, MessageOutlined } from "@ant-design/icons";
-import { Image, Layout, Menu } from "antd";
+import { Layout } from "antd";
 import { useEffect } from "react";
-
 import useFetch from "../../hooks/useFetch";
-
-import Logout from "../../components/Logout";
 
 import { useSelector } from "react-redux";
 import { Outlet, useNavigate } from "react-router-dom";
@@ -15,15 +11,21 @@ import Loading from "./../../components/Loading/Loading";
 import { store } from "./../../redux/store";
 import { io } from "socket.io-client";
 import { initializeSocket } from "../../configs/socketInstance";
+import "./MainPage.scss";
+
 import SideBar from "./SideBar";
+import ChatList from "../../components/ChatList/ChatList";
+import ChatWindow from "../../components/ChatWindow/ChatWindow";
+
+const { Content } = Layout;
 
 const MainPage = () => {
   const { isAuthenticated } = useSelector(authSelector);
   const { fetchData, isLoading, contextHolder } = useFetch();
-  const naviage = useNavigate();
-  const handleNavigatie = (e) => {
-    naviage(`/${e.key}`);
-  };
+  // const naviage = useNavigate();
+  // const handleNavigatie = (e) => {
+  //   naviage(`/${e.key}`);
+  // };
   useEffect(() => {
     (async () => {
       if (isAuthenticated) {
@@ -46,7 +48,16 @@ const MainPage = () => {
       {contextHolder}
       <Layout style={{ height: "100vh" }}>
         <SideBar />
-        <Logout />
+        <Layout>
+          <Content style={{ display: "flex", flexDirection: "row" }}>
+            <div className="chat-list-container">
+              <ChatList />
+            </div>
+            <div className="chat-window-container">
+              <ChatWindow />
+            </div>
+          </Content>
+        </Layout>
       </Layout>
     </>
   );
