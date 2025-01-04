3f645fc9a1f19ace6fa8c988c421abe71551da79
nguyn_huynh <huynhphuocnguyen2412@gmail.com>
2025-01-05 00:37:10 +0700

    hotfix v1

--- src/components/VideoCall/StreamsGrid.jsx
+++ src/components/VideoCall/StreamsGrid.jsx
@@ -38,12 +38,12 @@ const StreamsGrid = ({ peerStreams }) => {
                         onClick={() => handleSelectVideo(selectedVideo)}
                     />
                 </div>
-                <div className='h-full w-1/4 overflow-hidden'>
+                <div className='h-full w-1/4 flex-col gap-3 overflow-hidden'>
                     {peerStreams
                         .filter((peerStream) => peerStream.peerId !== selectedVideo.peerId)
                         .slice(0, 2)
                         .map((peerStream, index) => (
-                            <div key={peerStream.peerId || index} className='mb-2 h-1/3'>
+                            <div key={peerStream.peerId || index} className='h-1/3'>
                                 <VideoPlayer peerStream={peerStream} onClick={() => handleSelectVideo(peerStream)} />
                             </div>
                         ))}
